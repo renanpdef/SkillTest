b42b85644cc4ba541a3eaa0c1e179051c4af6b44
Renan Figueiredo <renanpdef@gmail.com>
2020-07-24 01:53:47 -0300

    Resolucao da Parte 1

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -13,5 +13,20 @@ class Exam < ApplicationRecord
 
   def maxillary_depth_angle
     return nil if self.point_po.x.nil? or self.point_po.y.nil? or self.point_or.x.nil? or self.point_or.y.nil? or self.point_n.x.nil? or self.point_n.y.nil? or self.point_a.x.nil? or self.point_a.y.nil?
+    m_line_a = slope_line(point_po, point_or)
+    m_line_b = slope_line(point_n, point_a)
+    angle = lines_angle(m_line_a, m_line_b)
+    maxillary_depth_angle = point_a.x < point_n.x ? angle : 180 - angle
+    return maxillary_depth_angle
+    end
+
+  def slope_line(point_a, point_b)
+    return (point_b.y - point_a.y) / (point_b.x - point_a.x)
+  end
+
+  def lines_angle(m_line_a, m_line_b)
+    tg_angle = (m_line_b - m_line_a)/(1 + m_line_b*m_line_a)
+    tg_angle = -tg_angle if tg_angle < 0
+    return Math.atan(tg_angle)*180/Math::PI
   end
 end
